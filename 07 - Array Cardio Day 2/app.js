db8c148684d54a56d8f2d8dde8300c16681c24c5
Andrzej Ruga <andrzej@ruga.pl>
2018-09-26 10:23:58 +0200

    Task 07 done

--- 07 - Array Cardio Day 2/app.js
+++ 07 - Array Cardio Day 2/app.js
@@ -15,12 +15,50 @@ const comments = [
 
 // Some and Every Checks
 // Array.prototype.some() // is at least one person 19 or older?
+const isAdult = people.some(item => {
+    const currYear = (new Date()).getFullYear();
+    if (currYear - item.year >= 19) {
+        return true;
+    }
+});
+console.log({isAdult});
+
 // Array.prototype.every() // is everyone 19 or older?
+const areAllAdults = people.every(item => {
+    const currYear = (new Date()).getFullYear();
+    return (currYear - item.year >= 19);
+});
+console.log({areAllAdults});
 
 // Array.prototype.find()
 // Find is like filter, but instead returns just the one you are looking for
 // find the comment with the ID of 823423
 
+// const thatComment = comments.find(item => {
+//     if (item.id === 823423) {
+//         return item;
+//     }
+// });
+// console.log(thatComment);
+
+// much shorter
+const thatComment = comments.find(item => item.id === 823423);
+console.log(thatComment);
+
 // Array.prototype.findIndex()
 // Find the comment with this ID
 // delete the comment with the ID of 823423
+const commentIndex = comments.findIndex(item => item.id === 823423);
+console.log(commentIndex);
+
+comments.splice(commentIndex, 1);
+console.table(comments);
+
+// alternative
+// const newComments = [
+//     ...comments.slice(0, commentIndex),
+//     ...comments.slice(commentIndex + 1)
+// ];
+// console.log(newComments);
+
+
