49dd618ef9e81f9fa134d91b0fb893f5e2c00242
Andrzej Ruga <andrzej@ruga.pl>
2018-10-02 18:08:27 +0200

    Task 13 completed

--- 13 - Slide in on Scroll/app.js
+++ 13 - Slide in on Scroll/app.js
@@ -1,3 +1,5 @@
+// function that runs our function only once every 20ms
+// so that our function that listens to scrolling doesn't run too often
 function debounce(func, wait = 20, immediate = true) {
     var timeout;
     return function () {
@@ -12,3 +14,23 @@ function debounce(func, wait = 20, immediate = true) {
         if (callNow) func.apply(context, args);
     };
 }
+
+const images = document.querySelectorAll('.slide-in');
+
+function checkSlide(e) {
+    images.forEach(item => {
+        // half way through the image
+        const slideInAt = (window.scrollY + window.innerHeight) - item.height / 2;
+        // bottom of the image
+        const imageBottom = item.offsetTop + item.height;
+        const isHalfShown = slideInAt > item.offsetTop;
+        const isNotScrolledPast = window.scrollY < imageBottom;
+        if (isHalfShown && isNotScrolledPast) {
+            item.classList.add('active');
+        } else {
+            item.classList.remove('active');
+        }
+    });
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
