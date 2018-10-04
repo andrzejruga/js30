990509421c660b1c901236ed3c56f69ef24f17ec
Andrzej Ruga <andrzej@ruga.pl>
2018-10-04 18:32:44 +0200

    Task 15 done

--- 15 - LocalStorage/app.js
+++ 15 - LocalStorage/app.js
@@ -1,3 +1,44 @@
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
-const items = [];
+const items = JSON.parse(localStorage.getItem('items')) || [];
+
+function addItem(e) {
+    e.preventDefault();
+    const text = (this.querySelector('[name = item]')).value;
+    const item = {
+        text: text,
+        // text, -> that is also correct since ES6 (without "text:")
+        done: false
+    };
+    items.push(item);
+    populateList(items, itemsList);
+    localStorage.setItem('items', JSON.stringify(items));
+    this.reset(); // clears input
+}
+
+function populateList(plates = [], platesList) {
+    platesList.innerHTML = plates.map((plate, index) => {
+        return `
+        <li>
+            <input type="checkbox" data-index=${index} id="item${index}" ${plate.done ? 'checked' : ''}/>
+            <label for="item${index}">${plate.text}</label>
+        </li>
+        `;
+    }).join('');
+}
+
+function toggleDone(e) {
+    if (!e.target.matches('input')) return; // skip target unless it's an input
+    console.log(e.target);
+    const element = e.target;
+    console.log(element.dataset.index);
+    const index = element.dataset.index;
+    items[index].done = !items[index].done;
+    localStorage.setItem('items', JSON.stringify(items));
+    populateList(items, itemsList);
+}
+
+addItems.addEventListener('submit', addItem);
+itemsList.addEventListener('click', toggleDone);
+
+populateList(items, itemsList);
