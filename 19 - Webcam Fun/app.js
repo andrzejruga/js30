5af13c8ecc9a53942d156b342dffb63fdf68fd08
Andrzej Ruga <andrzej@ruga.pl>
2018-10-08 16:11:50 +0200

    Task 19 done

--- 19 - Webcam Fun/app.js
+++ 19 - Webcam Fun/app.js
@@ -25,12 +25,48 @@ function paintToCanvas() {
 
     return setInterval(() => {
         ctx.drawImage(video, 0, 0, width, height);
+        // now we start messing with special effects
+        // take the pixels out
+        let pixels = ctx.getImageData(0, 0, width, height);
+        // mess with them
+        // pixels = redEffect(pixels);
+
+        pixels = rgbSplit(pixels);
+        // put them back
+        ctx.putImageData(pixels, 0, 0);
     }, 16);
 }
 
 function takePhoto() {
     snap.currentTime = 0;
     snap.play();
+    // this above plays the sound, now we'll take the data out of the canvas
+    const data = canvas.toDataURL('image/jpeg');
+    const link = document.createElement('a');
+    link.href = data;
+    link.setAttribute('download', 'handsome');
+    link.innerHTML = `<img src="${data}" alt="Handsome Man" />`;
+    strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+    for (let i=0; i<pixels.data.length; i+=4) {
+        pixels.data[i + 0] = pixels.data[i + 0] + 100; // red
+        pixels.data[i + 1] = pixels.data[i + 1] - 50; // green
+        pixels.data[i + 2] = pixels.data[i + 2] * 0,5; // blue
+    }
+    return pixels;
+}
+
+function rgbSplit(pixels) {
+    for (let i=0; i<pixels.data.length; i+=4) {
+        pixels.data[i - 150] = pixels.data[i + 0]; // red
+        pixels.data[i + 100] = pixels.data[i + 1]; // green
+        pixels.data[i - 150] = pixels.data[i + 2]; // blue
+    }
+    return pixels;
 }
 
 getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
