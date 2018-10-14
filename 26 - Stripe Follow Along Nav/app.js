74186fe9f84a853294c472f5b4d57bbdfba08547
Andrzej Ruga <andrzej@ruga.pl>
2018-10-14 17:23:22 +0200

    Halfway there, gotta leave for a while

--- 26 - Stripe Follow Along Nav/app.js
+++ 26 - Stripe Follow Along Nav/app.js
@@ -1,5 +1,25 @@
 document.addEventListener('DOMContentLoaded', function () {
 
+    const triggers = document.querySelectorAll('.cool > li');
+    const background = document.querySelector('.dropdownBackground');
+    const nav = document.querySelector('.top');
 
+    function handleEnter() {
+        this.classList.add('trigger-enter');
+        // setTimeout(function () {
+        //     this.classList.add('trigger-enter-active');
+        // }, 200);
+        // code above won't work, because "this" doesn't refer to trigger
+        // because it's in another function
+        // we need to use arrow function, then "this" inherits value from parent function
+        setTimeout(() => this.classList.add('trigger-enter-active'), 200);
+    }
+
+    function handleLeave() {
+        console.log('leave');
+    }
+
+    triggers.forEach(trigger => trigger.addEventListener('mouseenter', handleEnter));
+    triggers.forEach(trigger => trigger.addEventListener('mouseleave', handleLeave));
 
 });
