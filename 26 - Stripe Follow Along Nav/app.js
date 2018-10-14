48314a1604a7ee41bb397648c49da587042e7803
Andrzej Ruga <andrzej@ruga.pl>
2018-10-14 19:11:19 +0200

    Task 26 completed. This was really cool!

--- 26 - Stripe Follow Along Nav/app.js
+++ 26 - Stripe Follow Along Nav/app.js
@@ -12,11 +12,37 @@ document.addEventListener('DOMContentLoaded', function () {
         // code above won't work, because "this" doesn't refer to trigger
         // because it's in another function
         // we need to use arrow function, then "this" inherits value from parent function
-        setTimeout(() => this.classList.add('trigger-enter-active'), 200);
+
+        setTimeout(() => this.classList.contains('trigger-enter') && this.classList.add('trigger-enter-active'), 200);
+        // above is shorter version of if statement - boolean
+        // if 1st is true, then 2nd will run
+        // if 1st is false, then 2nd doesn't bother us
+        // setTimeout(() => {
+        //     if (this.classList.contains('trigger-enter')) {
+        //         this.classList.add('trigger-enter-active')
+        //     }
+        // }, 200);
+        background.classList.add('open');
+
+        const dropdown = this.querySelector('.dropdown');
+        const dropdownCoordinates = dropdown.getBoundingClientRect();
+        const navCoordinates = nav.getBoundingClientRect();
+
+        const coords = {
+            height: dropdownCoordinates.height,
+            width: dropdownCoordinates.width,
+            top: dropdownCoordinates.top - navCoordinates.top,
+            left: dropdownCoordinates.left - navCoordinates.left
+        };
+
+        background.style.width = `${coords.width}px`;
+        background.style.height = `${coords.height}px`;
+        background.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
     }
 
     function handleLeave() {
-        console.log('leave');
+        this.classList.remove('trigger-enter', 'trigger-enter-active');
+        background.classList.remove('open');
     }
 
     triggers.forEach(trigger => trigger.addEventListener('mouseenter', handleEnter));
