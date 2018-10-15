fbcf9706754b7bc6b3df312dce42c4b575e1a1be
Andrzej Ruga <andrzej@ruga.pl>
2018-10-15 16:41:38 +0200

    Task 27 completed

--- 27 - Click and Drag/app.js
+++ 27 - Click and Drag/app.js
@@ -1,5 +1,34 @@
 document.addEventListener('DOMContentLoaded', function () {
 
+    const slider = document.querySelector('.items');
+    let isDown = false;
+    let startX;
+    let scrollLeft;
 
+    slider.addEventListener('mousedown', (e) => {
+        isDown = true;
+        slider.classList.add('active');
+        startX = e.pageX - slider.offsetLeft;
+        scrollLeft = slider.scrollLeft;
+    });
+
+    slider.addEventListener('mouseleave', () => {
+        isDown = false;
+        slider.classList.remove('active');
+    });
+
+    slider.addEventListener('mouseup', () => {
+        isDown = false;
+        slider.classList.remove('active');
+    });
+
+    slider.addEventListener('mousemove', (e) => {
+        if (!isDown) return; // stop function from running
+        e.preventDefault();
+        const x = e.pageX - slider.offsetLeft;
+        const walk = x - startX;
+        // const walk = (x - startX) * 2.5; // scroll multiplier
+        slider.scrollLeft = scrollLeft - walk;
+    });
 
 });
