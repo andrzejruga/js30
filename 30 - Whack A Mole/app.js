8416de9d9350d0cce8c51a11bdc14797f1e49a2f
Andrzej Ruga <andrzej@ruga.pl>
2018-10-18 15:35:14 +0200

    Prepared Task 30 folder and files

---
+++ 30 - Whack A Mole/app.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    const holes = document.querySelectorAll('.hole');
+    const scoreBoard = document.querySelector('.score');
+    const moles = document.querySelectorAll('.mole');
+
+});
